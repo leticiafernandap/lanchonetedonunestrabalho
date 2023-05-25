2eabb4ebd6e459853d0c0a5a39595937fb24e499
MATHEUS HENRIQUE VITAL <matheusvital029@gmail.com>
2023-05-25 11:14:41 -0300

    jhfwf

--- pedido.php
+++ pedido.php
@@ -1,19 +1,19 @@
 <?php
 
-    include("conexao.php"); //arquivo php referente ao banco de dados
-    if(isset($_POST['bt_nome'])){
-        $nome = $_POST['bt_nome'];
-        $endereco = $_POST['bt_endereco'];
-        $telefone = $_POST['bt_telefone'];
-        $pedido = $_POST['bt_pedido'];
+include("conexao.php"); //arquivo php referente ao banco de dados
+if (isset($_POST['bt_nome'])) {
+    $nome = $_POST['bt_nome'];
+    $endereco = $_POST['bt_endereco'];
+    $telefone = $_POST['bt_telefone'];
+    $pedido = $_POST['bt_pedido'];
 
-        
-        
-       
 
-        $mysqli -> query("INSERT INTO lanchonetesubway_nunes (nome, endereco, telefone, pedido) values ('$nome' , '$endereco' , '$telefone' , '$pedido')") or
-    die ($mysqli ->error);
-    }
+
+
+
+    $mysqli->query("INSERT INTO lanchonetesubway_nunes (nome, endereco, telefone, pedido) values ('$nome' , '$endereco' , '$telefone' , '$pedido')") or
+        die($mysqli->error);
+}
 ?>
 
 
@@ -31,47 +31,9 @@
 </head>
 
 <body>
-<nav class="navbar bg-body-tertiary fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img src="img/logo1.png" alt="" width="240"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="img/logo1.png" alt="" width="170"></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include("menu.php");
+    ?>
     <br>
     <br>
     <br>
@@ -97,12 +59,12 @@
                 <label for="">Seu pedido:</label>
                 <input type="text" name="bt_pedido">
             </div>
-            
+
             <div class="mb-3">
-               <input class="btn btn-success" type="submit" value="pedir">
-               <a  class="btn btn-danger" href="index.php">Cancelar</a>
-               <a  class="btn btn-primary" href="listadepedidos.php">Pedidos</a>
-               
+                <input class="btn btn-success" type="submit" value="pedir">
+                <a class="btn btn-danger" href="index.php">Cancelar</a>
+                <a class="btn btn-primary" href="listadepedidos.php">Pedidos</a>
+
             </div>
 
 
